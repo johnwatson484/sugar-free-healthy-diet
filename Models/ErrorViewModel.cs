d0a1cf53d37db09eec398257a280fcca0968ddaf
John Watson <john.watson2@rpa.gov.uk>
2019-12-17 09:23:33 +0000

    Initial project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SugarFreeHealthyDiet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
