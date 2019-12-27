eec1cd7e38d62cf35714890c23cdaa4fbcbb1a0f
John Watson <johnwatson484@hotmail.com>
2019-12-27 15:04:40 +0000

    Redundant usings removed

--- SugarFreeHealthyDiet/Areas/Identity/IdentityHostingStartup.cs
+++ SugarFreeHealthyDiet/Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using SugarFreeHealthyDiet.Data;
 
 [assembly: HostingStartup(typeof(SugarFreeHealthyDiet.Areas.Identity.IdentityHostingStartup))]
 namespace SugarFreeHealthyDiet.Areas.Identity
--- SugarFreeHealthyDiet/Controllers/RecipesController.cs
+++ SugarFreeHealthyDiet/Controllers/RecipesController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Http;
 using SugarFreeHealthyDiet.Data;
 using SugarFreeHealthyDiet.Models;
